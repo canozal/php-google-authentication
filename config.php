5fb711d52647e5013c5eceaf43c2fdeee996f618
can <47246431+canozal@users.noreply.github.com>
2021-02-12 17:10:51 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//config.php
+
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId('<YOUR_CLIENT_ID>');
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret('<YOUR_CLIENT_SECRET>');
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri('<REDIRECT_URI>');
+
+//
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+//start session on web page
+session_start();
